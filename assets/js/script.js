fe874dbe8deea8f1a77ceae9cc1481ecdc256ac9
kuhena <pennant25video@gmail.com>
2024-05-06 02:16:24 +0900

    Fix: Anchor link activation

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,12 +19,52 @@ $(function(){
   });
 });
 
-$(function(){
-  $('a[href^="#"]').click(function(){//href属性に#を含むaタグをクリックした時
-    var href= $(this).attr("href");//クリックした要素のhref属性を取得
-    var target = $(href == "#" || href == "" ? 'html' : href);//リンク先を取得
-    var position = target.offset().top-50;//リンク先までの距離を取得
-    $("html, body").animate({scrollTop:position}, 300);//スムーススクロール
-    return false;
-  });
+//アンカーリンクの有効化
+$(function() {
+  // ナビゲーションのリンクを指定
+ var navLink = $('nav li a');
+
+  // 各コンテンツのページ上部からの開始位置と終了位置を配列に格納しておく
+ var contentsArr = new Array();
+for (var i = 0; i < navLink.length; i++) {
+     // コンテンツのIDを取得
+    var targetContents = navLink.eq(i).attr('href');
+    // ページ内リンクでないナビゲーションが含まれている場合は除外する
+    if(targetContents.charAt(0) == '#') {
+       // ページ上部からコンテンツの開始位置までの距離を取得
+          var targetContentsTop = $(targetContents).offset().top;
+       // ページ上部からコンテンツの終了位置までの距離を取得
+          var targetContentsBottom = targetContentsTop + $(targetContents).outerHeight(true) - 1;
+       // 配列に格納
+          contentsArr[i] = [targetContentsTop, targetContentsBottom]
+    }
+ };
+
+// 現在地をチェックする
+ function currentCheck() {
+     // 現在のスクロール位置を取得
+      var windowScrolltop = $(window).scrollTop();
+      for (var i = 0; i < contentsArr.length; i++) {
+         // 現在のスクロール位置が、配列に格納した開始位置と終了位置の間にあるものを調べる
+        if(contentsArr[i][0] <= windowScrolltop && contentsArr[i][1] >= windowScrolltop) {
+              // 開始位置と終了位置の間にある場合、ナビゲーションにclass="current"をつける
+             navLink.removeClass('current');
+             navLink.eq(i).addClass('current');
+              i == contentsArr.length;
+          }
+     };
+}
+
+ // ページ読み込み時とスクロール時に、現在地をチェックする
+$(window).on('load scroll', function() {
+    currentCheck();
+});
+
+// ナビゲーションをクリックした時のスムーズスクロール
+  navLink.click(function() {
+    $('html,body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+     }, 300);
+      return false;
+ })
 });
