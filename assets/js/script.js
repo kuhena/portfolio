c2c349117b661f7c480538fd5ced29e6b6ef3f77
kuhena <pennant25video@gmail.com>
2024-05-06 02:12:05 +0900

    Fix: Anchor link activation

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,13 @@ $(function(){
       });
   });
 });
+
+$(function(){
+  $('a[href^="#"]').click(function(){//href属性に#を含むaタグをクリックした時
+    var href= $(this).attr("href");//クリックした要素のhref属性を取得
+    var target = $(href == "#" || href == "" ? 'html' : href);//リンク先を取得
+    var position = target.offset().top-50;//リンク先までの距離を取得
+    $("html, body").animate({scrollTop:position}, 300);//スムーススクロール
+    return false;
+  });
+});
