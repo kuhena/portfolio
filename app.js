bd756ee2a831e86b20394e1b81436dfb75717e9a
kuhena <pennant25video@gmail.com>
2024-05-08 20:46:01 +0900

    Add: Add file for basic authentication setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const protect = require('static-auth');
+const safeCompare = require('safe-compare');
+
+const USER_NAME = process.env.USER_NAME || 'admin';
+const PASSWORD = process.env.PASSWORD || 'pass';
+
+const app = protect(
+  '/',
+  (username, password) => safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD),
+  {
+    directory: `${__dirname}/dist`, // dist 配下のファイルを静的コンテンツとして配信する
+    onAuthFailed: (res) => {
+      res.end('Authentication failed')
+    },
+  }
+)
+
+module.exports = app
