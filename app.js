47df3f1bbbbf48ab183e57c44c9abdaa2455dc02
kuhena <pennant25video@gmail.com>
2024-05-08 21:18:48 +0900

    vercel call test

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const protect = require('static-auth');
-const safeCompare = require('safe-compare');
+// const protect = require('static-auth');
+// const safeCompare = require('safe-compare');
 
-const USER_NAME = process.env.USER_NAME || 'admin';
-const PASSWORD = process.env.PASSWORD || 'pass';
+// const USER_NAME = process.env.USER_NAME || 'admin';
+// const PASSWORD = process.env.PASSWORD || 'pass';
 
-const app = protect(
-  '/',
-  (username, password) => safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD),
-  {
-    directory: `${__dirname}`, // dist 配下のファイルを静的コンテンツとして配信する
-    onAuthFailed: (res) => {
-      res.end('Authentication failed')
-    },
-  }
-)
+// const app = protect(
+//   '/',
+//   (username, password) => safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD),
+//   {
+//     directory: `${__dirname}`, // dist 配下のファイルを静的コンテンツとして配信する
+//     onAuthFailed: (res) => {
+//       res.end('Authentication failed')
+//     },
+//   }
+// )
 
 console.log('test')
-module.exports = app
+// module.exports = app
